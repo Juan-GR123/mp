cf47e476ce51e7da5f79212a3c41a724a84924ca
Juan-GR123 <juangarros2005@gmail.com>
2026-02-22 19:01:14 +0100

    mp inicializar

---
+++ nuevo.cpp
@@ -0,0 +1,22 @@
+/****************************************************************/
+// FUNDAMENTOS DE PROGRAMACI�N
+//
+// (C) FRANCISCO JOS� CORTIJO BON
+// DPTO. DE CIENCIAS DE LA COMPUTACI�N E INTELIGENCIA ARTIFICIAL
+// 
+/* 
+	Programa para mostrar un mensaje de saludo.
+*/
+/****************************************************************/
+
+#include <iostream>   // Inclusi�n de los recursos de E/S
+using namespace std; 
+
+int main () // Programa Principal
+{                       
+	cout << endl;
+	cout << "!Hola, mundo!" << endl;
+	cout << endl;
+
+   return 0;
+}
