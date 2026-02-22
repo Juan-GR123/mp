8a90082adc227eb0f1ba946904e10d107aba447b
Juan-GR123 <juangarros2005@gmail.com>
2026-02-22 19:02:40 +0100

    NUEVO

--- nuevo.cpp
+++ nuevo.cpp
@@ -1,12 +1,5 @@
 /****************************************************************/
-// FUNDAMENTOS DE PROGRAMACI�N
-//
-// (C) FRANCISCO JOS� CORTIJO BON
-// DPTO. DE CIENCIAS DE LA COMPUTACI�N E INTELIGENCIA ARTIFICIAL
-// 
-/* 
-	Programa para mostrar un mensaje de saludo.
-*/
+// METODOLOGIA DE LA PROGRAMACION
 /****************************************************************/
 
 #include <iostream>   // Inclusi�n de los recursos de E/S
